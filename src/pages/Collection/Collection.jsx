d4839d97f7cfd8b9d1e2bf7385a1acd1facd7b09
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-01-14 19:13:16 +0100

    feat(collection): adding anime.js and add animation

--- src/pages/Collection/Collection.jsx
+++ src/pages/Collection/Collection.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import anime from 'animejs/lib/anime.es.js';
 import './Collection.css';
 
 export function Collection() {
@@ -6,6 +7,7 @@ export function Collection() {
     useEffect(() => {
         document.querySelector(".nav-top").classList.add("active");
         document.querySelector(".nav-bottom").classList.add("active");
+        document.body.style.overflow = "hidden";
 
         const collection = document.getElementById("collection");
 
@@ -30,13 +32,28 @@ export function Collection() {
             easing: "ease"
           })
         }
+
         const tile = document.querySelectorAll(".tile");
         const title = document.querySelector(".Collection-tittle");
+        var textWrapper = document.querySelector('.ml13');
 
         tile.forEach(element => {
             element.addEventListener("mouseover", () => {
-                title.classList.add("active");
+
+                
                 title.innerHTML = element.dataset.title;
+                textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+                title.classList.add("active");
+                anime.timeline()
+                .add({
+                    targets: '.ml13 .letter',
+                    translateY: [80, 0],
+                    translateZ: 0,
+                    opacity: [0, 1],
+                    easing: "easeOutExpo",
+                    duration: 2500,
+                    delay: (el, i) => + 30 * i
+                });
             });
             element.addEventListener("mouseout", () => {
                 title.classList.remove("active");
@@ -47,33 +64,33 @@ export function Collection() {
 
     return (
         <>
-        <h1 className='Collection-tittle'>SO-art</h1>
+        <h1 className='Collection-tittle ml13'>Collection</h1>
         <div id="collection">
-            <div className="tile" data-title='Artingo'>
+            <div className="tile" data-title='Collection 1'>
                 <img src="https://images.unsplash.com/photo-1481349518771-20055b2a7b24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjF8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 2'>
                 <img src="https://images.unsplash.com/photo-1515266591878-f93e32bc5937?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsdWV8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 3'>
                 <img src="https://images.unsplash.com/photo-1587590227264-0ac64ce63ce8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 4'>
                 <img src="https://images.unsplash.com/photo-1520121401995-928cd50d4e27?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Z3JlZW58ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 5'>
                 <img src="https://images.unsplash.com/photo-1511447333015-45b65e60f6d5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHVycGxlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 6'>
                 <img src="https://images.unsplash.com/photo-1557800636-894a64c1696f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8b3JhbmdlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 7'>
                 <img src="https://images.unsplash.com/photo-1520338258525-606b90f95b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTR8fGRhcmslMjBibHVlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 8'>
                 <img src="https://images.unsplash.com/photo-1521127474489-d524412fd439?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHJhbmRvbSUyMG9iamVjdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=70" />
             </div>
-            <div className="tile" data-title='Newera'>
+            <div className="tile" data-title='Collection 9'>
                 <img src="https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmFuZG9tJTIwb2JqZWN0c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=70" />
             </div>
         </div>
