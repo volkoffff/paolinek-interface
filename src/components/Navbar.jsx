75e995a58085091e886ab3c3c7ddd03d85f437e6
volkoffff <robin.lanfranchi@etudiant.univ-reims.fr>
2024-01-18 18:07:01 +0100

    feat(navbar): adding (menu) when scroling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Navbar() {
+    const navigate = useNavigate();
 
     window.onscroll = function() {
         if (window.pageYOffset > 40) {
@@ -10,18 +14,32 @@ export function Navbar() {
             document.querySelector(".nav-bottom").classList.remove("active");
         }
     };
+    
+    useEffect(() => {
+
+        const menuInfo = document.querySelector(".menu-info");
+
+        window.onscroll = function() {
+            menuInfo.classList.add("active");    
+        };
+        window.onscrollend = function() {
+            setTimeout(() => {
+                menuInfo.classList.remove("active");    
+            }, 2000);
+        };
+    }, []);
 
     return (
         <>
             <nav className="nav-top">
                 <div className="navbar">
                     <div className="navbar_box navbar_left">
-                        <p className="link text-main">Paoline.K</p>
-                        <p>Collage</p>
+                        <p className="link text-main" onClick={() => {navigate('/')}}>Paoline.K</p>
+                        <p className="pl35">Collage</p>
                     </div>
-                    <div className="navbar_box navbar_logo">
-                        {/* <img className="logo link" src="img/Group 1.svg" alt=""> */}
-                        <p className=''>PK</p>
+                    <div className="navbar_box navbar_right">
+                        <div className="navbar_box menu-info">(Menu)</div>
+                        <p className='link navbar_logo' onClick={() => {navigate('/')}}>PK</p>
                     </div>
                 </div>
             </nav>
@@ -29,8 +47,9 @@ export function Navbar() {
                 <div className="navbar">
                     <div className="navbar_box navbar_box2 navbar_left">
                         <p>content of an artist </p>
-                        <p className='text-main'>for everyone </p>
+                        <p className='text-main pl35'>for everyone </p>
                     </div>
+
                 </div>
             </nav>
         </>
